9e4d63f52d82c122690cc84c7f2b5b3101b42570
knight <karvin_jimenez@hotmail.com>
2024-08-29 14:39:05 -0400

    added collisions

--- source/main.cpp
+++ source/main.cpp
@@ -20,6 +20,12 @@ typedef struct
 	unsigned int color;
 } Rectangle;
 
+bool hasCollision(Rectangle bounds, Rectangle ball)
+{
+	return bounds.x < ball.x + ball.w && bounds.x + bounds.w > ball.x &&
+		   bounds.y < ball.y + ball.h && bounds.y + bounds.h > ball.y;
+}
+
 int main(int argc, char *argv[])
 {
 	// Init libs
@@ -38,14 +44,21 @@ int main(int argc, char *argv[])
 
 	// Load the sprite
 	C2D_SpriteSheet sheet = C2D_SpriteSheetLoad("romfs:/gfx/alien_1.t3x");
-	C2D_Image bird = C2D_SpriteSheetGetImage(sheet, 0);
+	C2D_Image sprite = C2D_SpriteSheetGetImage(sheet, 0);
 
 	// Create colors
 	u32 whiteColor = C2D_Color32(0xFF, 0xFF, 0xFF, 0xFF);
-
 	u32 blackColor = C2D_Color32(0x00, 0x00, 0x00, 0x00);
+	// u32 greenColor = C2D_Color32(0x00, 0xFF, 0x00, 0xFF);
+	// u32 redColor = C2D_Color32(0xFF, 0x00, 0x00, 0xFF);
+	u32 blueColor  = C2D_Color32(0x00, 0x00, 0xFF, 0xFF);
 
-	Rectangle bounds = {BOTTOM_SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0, 32, 32, whiteColor};
+	Rectangle spriteBounds = {TOP_SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0, 32, 32, whiteColor};
+	Rectangle ball = {TOP_SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2, 0, 20, 20, whiteColor};
+	Rectangle bottomBall = {BOTTOM_SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0, 32, 32, blueColor};
+
+	int ballVelocityX = 5;
+	int ballVelocityY = 5;
 
 	// Main loop
 	while (aptMainLoop())
@@ -61,30 +74,60 @@ int main(int argc, char *argv[])
 		if (keyDown & KEY_START)
 			break; // break in order to return to hbmenu
 
-		if (keyHeld & KEY_LEFT && bounds.x > 0)
+		if (keyHeld & KEY_LEFT && spriteBounds.x > 0)
 		{
-			bounds.x -= SPEED;
+			spriteBounds.x -= SPEED;
 		}
-		if (keyHeld & KEY_RIGHT && bounds.x < BOTTOM_SCREEN_WIDTH - bounds.w)
+
+		else if (keyHeld & KEY_RIGHT && spriteBounds.x < TOP_SCREEN_WIDTH - spriteBounds.w)
 		{
-			bounds.x += SPEED;
+			spriteBounds.x += SPEED;
 		}
-		if (keyHeld & KEY_UP && bounds.y > 0)
+		else if (keyHeld & KEY_UP && spriteBounds.y > 0)
 		{
-			bounds.y -= SPEED;
+			spriteBounds.y -= SPEED;
 		}
-		if (keyHeld & KEY_DOWN && bounds.y < SCREEN_HEIGHT - bounds.h)
+		else if (keyHeld & KEY_DOWN && spriteBounds.y < SCREEN_HEIGHT - spriteBounds.h)
 		{
-			bounds.y += SPEED;
+			spriteBounds.y += SPEED;
 		}
+
+		// bottom screen update
+		if (ball.x < 0 || ball.x > TOP_SCREEN_WIDTH - ball.w)
+		{
+			ballVelocityX *= -1;
+
+			// colorIndex = rand_range(0, 5);
+		}
+
+		else if (ball.y < 0 || ball.y > SCREEN_HEIGHT - ball.h)
+		{
+			ballVelocityY *= -1;
+
+			// colorIndex = rand_range(0, 5);
+		}
+
+		else if (hasCollision(spriteBounds, ball))
+		{
+			ballVelocityX *= -1;
+			ballVelocityY *= -1;
+
+			// colorIndex = rand_range(0, 5);
+		}
+
+		ball.x += ballVelocityX;
+		ball.y += ballVelocityY;
 
 		// Render the scene
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 		C2D_TargetClear(top, blackColor);
 		C2D_SceneBegin(top);
 
-		// ( 	C2D_Image img, float x, float y, float depth, const C2D_ImageTint *tint C2D_OPTIONALnullptr, float scaleX C2D_OPTIONAL1.0f, float scaleY C2D_OPTIONAL1.0f) 	
-		C2D_DrawImageAt(bird, 136, 56, 0, NULL, 1, 1);
+		// (float x, float y, float z, float  w, float h, u32 clr)
+		C2D_DrawRectSolid(ball.x, ball.y, ball.z, ball.w, ball.h, ball.color);
+
+		// ( 	C2D_Image img, float x, float y, float depth, const C2D_ImageTint *tint C2D_OPTIONALnullptr, float scaleX C2D_OPTIONAL1.0f, float scaleY C2D_OPTIONAL1.0f)
+		C2D_DrawImageAt(sprite, spriteBounds.x, spriteBounds.y, 0, NULL, 1, 1);
 
 		C3D_FrameEnd(0);
 
@@ -92,8 +135,7 @@ int main(int argc, char *argv[])
 		C2D_TargetClear(bottom, blackColor);
 		C2D_SceneBegin(bottom);
 
-		// (float x, float y, float z, float  w, float h, u32 clr)
-		C2D_DrawRectSolid(bounds.x, bounds.y, bounds.z, bounds.w, bounds.h, bounds.color);
+		C2D_DrawRectSolid(bottomBall.x, bottomBall.y, bottomBall.z, bottomBall.w, bottomBall.h, bottomBall.color);
 
 		C3D_FrameEnd(0);
 	}
