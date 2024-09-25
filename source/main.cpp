4757f48106b62b66caa146f9fb5dc19c87bc9b92
knight <karvin_jimenez@hotmail.com>
2024-09-24 21:07:31 -0400

    extra changes.

--- source/main.cpp
+++ source/main.cpp
@@ -5,6 +5,9 @@ const int TOP_SCREEN_WIDTH = 400;
 const int BOTTOM_SCREEN_WIDTH = 320;
 const int SCREEN_HEIGHT = 240;
 
+C3D_RenderTarget *topScreen = nullptr;
+C3D_RenderTarget *bottomScreen = nullptr;
+
 bool isGamePaused;
 
 int collisionCounter;
@@ -13,6 +16,10 @@ C2D_TextBuf textDynamicBuffer;
 
 C2D_TextBuf textStaticBuffer;
 C2D_Text staticTexts[1];
+
+float textSize = 1.0f;
+
+C2D_Image sprite;
 
 // Create colors
 const u32 WHITE = C2D_Color32(0xFF, 0xFF, 0xFF, 0xFF);
@@ -98,6 +105,58 @@ void update()
 	ball.y += ballVelocityY;
 }
 
+void renderTopScreen()
+{
+	C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+	C2D_TargetClear(topScreen, BLACK);
+	C2D_SceneBegin(topScreen);
+
+	// (float x, float y, float z, float  w, float h, u32 clr)
+	C2D_DrawRectSolid(ball.x, ball.y, ball.z, ball.w, ball.h, ball.color);
+
+	// ( 	C2D_Image img, float x, float y, float depth, const C2D_ImageTint *tint C2D_OPTIONALnullptr, float scaleX C2D_OPTIONAL1.0f, float scaleY C2D_OPTIONAL1.0f)
+	C2D_DrawImageAt(sprite, spriteBounds.x, spriteBounds.y, 0, NULL, 1, 1);
+
+	if (isGamePaused)
+	{
+		// Draws text using the GPU.
+
+		// Parameters
+		//     [in]	text	Pointer to text object.
+		//     [in]	flags	Text drawing flags.
+		//     [in]	x	Horizontal position to draw the text on.
+		//     [in]	y	Vertical position to draw the text on. If C2D_AtBaseline is not specified (default), this is the top left corner of the block of text; otherwise this is the position of the baseline of the first line of text.
+		//     [in]	z	Depth value of the text. If unsure, pass 0.0f.
+		//     [in]	scaleX	Horizontal textSize of the font. 1.0f corresponds to the native textSize of the font.
+		//     [in]	scaleY	Vertical textSize of the font. 1.0f corresponds to the native textSize of the font.
+		// Draw static text strings
+		C2D_DrawText(&staticTexts[0], C2D_AtBaseline | C2D_WithColor, 110, 60, 0, textSize, textSize, WHITE);
+	}
+
+	C3D_FrameEnd(0);
+}
+
+void renderBottomScreen()
+{
+	C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+	C2D_TargetClear(bottomScreen, BLACK);
+	C2D_SceneBegin(bottomScreen);
+
+	C2D_DrawRectSolid(bottomBounds.x, bottomBounds.y, bottomBounds.z, bottomBounds.w, bottomBounds.h, bottomBounds.color);
+
+	C2D_TextBufClear(textDynamicBuffer);
+
+	// Generate and draw dynamic text
+	char buf[160];
+	C2D_Text dynamicText;
+	snprintf(buf, sizeof(buf), "Total collisions: %d", collisionCounter);
+	C2D_TextParse(&dynamicText, textDynamicBuffer, buf);
+	C2D_TextOptimize(&dynamicText);
+	C2D_DrawText(&dynamicText, C2D_AlignCenter | C2D_WithColor, 150, 175, 0, textSize, textSize, WHITE);
+
+	C3D_FrameEnd(0);
+}
+
 int main(int argc, char *argv[])
 {
 	// Init libs
@@ -108,8 +167,8 @@ int main(int argc, char *argv[])
 	C2D_Prepare();
 
 	// Create top and bottom screens
-	C3D_RenderTarget *topScreen = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
-	C3D_RenderTarget *bottomScreen = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
+	topScreen = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
+	bottomScreen = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
 
 	// Create two text buffers: one for static text, and another one for
 	// dynamic text - the latter will be cleared at each frame.
@@ -124,9 +183,7 @@ int main(int argc, char *argv[])
 
 	// Load the sprite
 	C2D_SpriteSheet sheet = C2D_SpriteSheetLoad("romfs:/gfx/alien_1.t3x");
-	C2D_Image sprite = C2D_SpriteSheetGetImage(sheet, 0);
-
-	float textSize = 1.0f;
+	sprite = C2D_SpriteSheetGetImage(sheet, 0);
 
 	touchPosition touch;
 
@@ -168,53 +225,9 @@ int main(int argc, char *argv[])
 			update();
 		}
 
-		// Render the top screen
-		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-		C2D_TargetClear(topScreen, BLACK);
-		C2D_SceneBegin(topScreen);
+		renderTopScreen();
 
-		// (float x, float y, float z, float  w, float h, u32 clr)
-		C2D_DrawRectSolid(ball.x, ball.y, ball.z, ball.w, ball.h, ball.color);
-
-		// ( 	C2D_Image img, float x, float y, float depth, const C2D_ImageTint *tint C2D_OPTIONALnullptr, float scaleX C2D_OPTIONAL1.0f, float scaleY C2D_OPTIONAL1.0f)
-		C2D_DrawImageAt(sprite, spriteBounds.x, spriteBounds.y, 0, NULL, 1, 1);
-
-		if (isGamePaused)
-		{
-			// Draws text using the GPU.
-
-			// Parameters
-			//     [in]	text	Pointer to text object.
-			//     [in]	flags	Text drawing flags.
-			//     [in]	x	Horizontal position to draw the text on.
-			//     [in]	y	Vertical position to draw the text on. If C2D_AtBaseline is not specified (default), this is the top left corner of the block of text; otherwise this is the position of the baseline of the first line of text.
-			//     [in]	z	Depth value of the text. If unsure, pass 0.0f.
-			//     [in]	scaleX	Horizontal textSize of the font. 1.0f corresponds to the native textSize of the font.
-			//     [in]	scaleY	Vertical textSize of the font. 1.0f corresponds to the native textSize of the font.
-			// Draw static text strings
-			C2D_DrawText(&staticTexts[0], C2D_AtBaseline | C2D_WithColor, 110, 60, 0, textSize, textSize, WHITE);
-		}
-
-		C3D_FrameEnd(0);
-
-		// Render the bottom screen
-		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-		C2D_TargetClear(bottomScreen, BLACK);
-		C2D_SceneBegin(bottomScreen);
-
-		C2D_DrawRectSolid(bottomBounds.x, bottomBounds.y, bottomBounds.z, bottomBounds.w, bottomBounds.h, bottomBounds.color);
-
-		C2D_TextBufClear(textDynamicBuffer);
-
-		// Generate and draw dynamic text
-		char buf[160];
-		C2D_Text dynamicText;
-		snprintf(buf, sizeof(buf), "Total collisions: %d", collisionCounter);
-		C2D_TextParse(&dynamicText, textDynamicBuffer, buf);
-		C2D_TextOptimize(&dynamicText);
-		C2D_DrawText(&dynamicText, C2D_AlignCenter | C2D_WithColor, 150, 175, 0, textSize, textSize, WHITE);
-
-		C3D_FrameEnd(0);
+		renderBottomScreen();
 	}
 
 	// Delete the text buffers
